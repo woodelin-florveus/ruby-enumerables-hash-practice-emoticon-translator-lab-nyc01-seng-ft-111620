089cf4ed67699f6549175a942e32e775c29f0f74
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-12 20:48:03 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -32,7 +32,7 @@ emoticon_hash = load_library(path)
 
 emoticon_result = emoticon_hash.keys.find do |key|
 
-  emoticon_hash[key][:english] == emoticon
+  emoticon_hash[key][0][:english] == emoticon
 end
 
 

0c0f283ff15b2ea1c76b33fe399340cc869b5344
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-11-12 22:09:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -29,10 +29,10 @@ end
 def get_japanese_emoticon(path, emoticon)
 
 emoticon_hash = load_library(path)
-
-emoticon_result = emoticon_hash.keys.find do |value|
+# binding.pry
+emoticon_result = emoticon_hash.keys.find do |key|
   emoticon_hash[key][:english] == emoticon
-  # binding.pry
+  
 end
 
 
